c4b9c76a9b0c32095d6ead8149c5a348c74b63b4
Kedar <kedar.gn20@gmail.com>
2016-10-19 01:58:13 -0400

    forecast new repo

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,16 +1,3 @@
-echo 'starting installation process' >> /var/log/sga-npcomplete-forecast-decision-install.log
-cd '/home/ec2-user/ForecastDecision'
-
-echo 'Activating virtualenv for ForecastDecision Microservice' >> /var/log/sga-npcomplete-forecast-decision-install.log
-pip install virtualenv >> /var/log/sga-npcomplete-forecast-decision-install.log
-virtualenv venv >> /var/log/sga-npcomplete-forecast-decision-install.log
-. venv/bin/activate >> /var/log/sga-npcomplete-forecast-decision-install.log
-pip install requests
-pip install Flask >> /var/log/sga-npcomplete-forecast-decision-install.log
-#echo 'Running Flask Server' >> /var/log/sga-npcomplete-flask-install.log
-export FLASK_APP=forecast_decision.py
-flask run --host=0.0.0.0 --port=65000 >> /var/log/sga-npcomplete-forecast-decision-server.log 2>&1 &
-
 cd '/home/ec2-user/Forecast'
 echo 'Activating virtualenv for Forecast Microservice' >> /var/log/sga-npcomplete-forecast-install.log
 pip install virtualenv >> /var/log/sga-npcomplete-forecast-install.log
