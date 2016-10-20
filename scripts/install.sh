09b5ffbd8fa2bf3c69b1dd018fc3b4daf60774c5
Kedar <kedar.gn20@gmail.com>
2016-10-20 05:23:53 -0400

    fixed port no

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@ echo 'starting installation process' >> /var/log/sga-npcomplete-forecast-decisio
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
 sudo docker pull kedargn/forecast
-sudo docker run -d -p 65000:65000 --name forecast $(sudo docker images | grep kedargn/forecast | awk '{print $3}') >> ./log.txt
+sudo docker run -d -p 64000:64000 --name forecast $(sudo docker images | grep kedargn/forecast | awk '{print $3}') >> ./log.txt
