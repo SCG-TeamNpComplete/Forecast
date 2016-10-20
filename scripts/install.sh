9140aa05368d0d98f9fb76ce1574996d1b5b9d6a
Kedar <kedar.gn20@gmail.com>
2016-10-20 05:47:15 -0400

    changed repo name

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 echo 'starting installation process' >> /var/log/sga-npcomplete-forecast-decision-install.log
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
-sudo docker pull kedargn/forecast
-sudo docker run -d -p 64000:64000 --name forecast $(sudo docker images | grep kedargn/forecast | awk '{print $3}') >> ./log.txt
+sudo docker pull kedargn/forecastrun
+sudo docker run -d -p 64000:64000 --name forecastrun $(sudo docker images | grep kedargn/forecastrun | awk '{print $3}') >> ./log.txt
