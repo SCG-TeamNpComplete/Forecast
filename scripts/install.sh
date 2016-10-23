5cb85c10afd5336c7db6ea824b6c943e698c1b69
Kedar <kedar.gn20@gmail.com>
2016-10-23 03:33:48 -0400

    test environment settings

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 echo 'starting installation process' >> /var/log/sga-npcomplete-forecast-decision-install.log
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
-sudo docker pull kedargn/forecastrun
-sudo docker run -d -p 64000:64000 --name forecastrun $(sudo docker images | grep kedargn/forecastrun | awk '{print $3}') >> ./log.txt
+sudo docker pull kedargn/forecastruntest
+sudo docker run -d -p 64000:64000 --name forecastruntest $(sudo docker images | grep kedargn/forecastruntest | awk '{print $3}') >> ./log.txt
