6df810315602b903e87320579ce9b8d6fc18084c
Kedar <kedar.gn20@gmail.com>
2016-11-15 01:01:44 -0500

    replication issue #2

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,15 @@
 echo 'starting installation process' >> /var/log/sga-npcomplete-forecast-decision-install.log
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
-sudo docker pull kedargn/forecastruntest
-sudo docker run -d -p 64000:64000 --name forecastruntest $(sudo docker images | grep kedargn/forecastruntest | awk '{print $3}') >> ./log.txt
+sudo docker pull kedargn/forecastrun
+
+#sudo docker ps -a | grep 'forecastrun1' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+#sudo docker ps -a | grep 'forecastrun1' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+sudo docker run -d -p 64000:64000 --name forecastrun1 $(sudo docker images | grep kedargn/forecastrun | awk '{print $3}') >> ./log.txt
+
+sleep 2
+
+#sudo docker ps -a | grep 'forecastrun2' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+#sudo docker ps -a | grep 'forecastrun2' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+sudo docker run -d -p 64001:64001 --name forecastrun2 $(sudo docker images | grep kedargn/forecastrun | awk '{print $3}') >> ./log.txt
+
