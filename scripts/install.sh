49ab436c113d866d935519d851f73c3c52c6f5d4
Kedar <kedar.gn20@gmail.com>
2016-11-15 02:30:12 -0500

    deploy 2 instances per VM

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,13 +3,13 @@ cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
 sudo docker pull kedargn/forecastrun
 
-#sudo docker ps -a | grep 'forecastrun1' | awk '{print $1}' | xargs --no-run-if-empty docker stop
-#sudo docker ps -a | grep 'forecastrun1' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+sudo docker ps -a | grep 'forecastrun1' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+sudo docker ps -a | grep 'forecastrun1' | awk '{print $1}' | xargs --no-run-if-empty docker rm
 sudo docker run -d -p 64000:64000 --name forecastrun1 $(sudo docker images | grep kedargn/forecastrun | awk '{print $3}') >> ./log.txt
 
 sleep 2
 
-#sudo docker ps -a | grep 'forecastrun2' | awk '{print $1}' | xargs --no-run-if-empty docker stop
-#sudo docker ps -a | grep 'forecastrun2' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+sudo docker ps -a | grep 'forecastrun2' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+sudo docker ps -a | grep 'forecastrun2' | awk '{print $1}' | xargs --no-run-if-empty docker rm
 sudo docker run -d -p 64001:64001 --name forecastrun2 $(sudo docker images | grep kedargn/forecastrun | awk '{print $3}') >> ./log.txt
 
