2f6536728a52d9471e1dc4ea97e0a9283e207d71
Kedar <kedar.gn20@gmail.com>
2016-11-16 01:11:55 -0500

    scale instances #2

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,24 @@
+#!/bin/bash
+
 echo 'starting installation process' >> /var/log/sga-npcomplete-forecast-decision-install.log
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
 sudo docker pull kedargn/forecastrun
 
-sudo docker ps -a | grep 'forecastrun1' | awk '{print $1}' | xargs --no-run-if-empty docker stop
-sudo docker ps -a | grep 'forecastrun1' | awk '{print $1}' | xargs --no-run-if-empty docker rm
-sudo docker run -d -p 64000:64000 --name forecastrun1 $(sudo docker images | grep kedargn/forecastrun | awk '{print $3}') >> ./log.txt
-
-sleep 2
-
-sudo docker ps -a | grep 'forecastrun2' | awk '{print $1}' | xargs --no-run-if-empty docker stop
-sudo docker ps -a | grep 'forecastrun2' | awk '{print $1}' | xargs --no-run-if-empty docker rm
-sudo docker run -d -p 64001:64001 --name forecastrun2 $(sudo docker images | grep kedargn/forecastrun | awk '{print $3}') >> ./log.txt
-
+no_of_instances=3     #change this to set the number of instances
+current_instance=1
+port=64000
+while [ $current_instance -le $no_of_instances ]
+do
+	#echo "$(sudo docker ps -a | grep "forecastdetector$current_instance" | awk '{print $1}')"
+	if [ "$(sudo docker ps -a | grep "forecastrun$current_instance" | awk '{print $1}')" != "" ]; then
+		sudo docker ps -a | grep "forecastrun$current_instance" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+		sudo docker ps -a | grep "forecastrun$current_instance" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+	fi
+	#echo "$port:$port"
+	sudo docker run -d -p "$port:$port" --name "forecastrun$current_instance" $(sudo docker images | grep kedargn/forecastrun | awk '{print $3}')
+	echo "forecastrun$current_instance instance"
+	current_instance=$((current_instance+1))
+	port=$((port+1))
+	sleep 1
+done
